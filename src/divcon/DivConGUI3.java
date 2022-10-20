247b2818567c0de019391ac2b07878e492b375cd
Eduardocesn <ceduardoadustina@gmail.com>
2022-10-19 23:49:31 -0300

    add nova tela de login na GUI v3

---
+++ src/divcon/DivConGUI3.java
@@ -0,0 +1,104 @@
+package divcon;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class DivConGUI3 {
+    private static final String VERSAO = "Versão 0.1";
+    private JFrame frame;
+    // private  PainelConta contaPainel;
+    private JLabel infoLabel;
+    private DivCon appDivCon;
+    private JTextField txtTeste;
+    private JLabel logoImgLabel;
+    private JLabel lblImgLogo;
+    private JPanel baixo;
+    private TelaLogin telaLogin;
+    
+
+    public static void main(String[] args) {
+        DivConGUI3 window = new DivConGUI3();
+        window.frame.setVisible(true);
+    }
+
+    public DivConGUI3() { 
+        appDivCon = new DivCon();
+        telaLogin = new TelaLogin();
+        makeFrame();
+    }
+
+    private void mostraCadastro() {
+        return;
+    }
+
+    private void makeFrame() {
+    	telaLogin.setVisible(true);
+        frame = new JFrame("DivCon");
+        // Obtemos o contentPane
+        JPanel contentPane = (JPanel)frame.getContentPane();
+        contentPane.setLayout(new BorderLayout(6, 6));
+        // Construímos o menu
+        makeMenuBar(frame);
+        // Colocamos um JLabel para mostrar o status da aplicação pro usuário
+        infoLabel = new JLabel(VERSAO);
+        baixo = new JPanel();
+ 
+        //O border south foi dividido em um gridbag layout, armazenando a logo png e o infolabel
+        contentPane.add(baixo, BorderLayout.SOUTH);
+        GridBagLayout gbl_baixo = new GridBagLayout();
+        gbl_baixo.columnWidths = new int[]{0, 0, 0, 0, 0, 0};
+        gbl_baixo.rowHeights = new int[]{0, 0};
+        gbl_baixo.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+        gbl_baixo.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+        baixo.setLayout(gbl_baixo);
+        
+        GridBagConstraints gbc_infoLabel = new GridBagConstraints();
+        gbc_infoLabel.fill = GridBagConstraints.BOTH;
+        gbc_infoLabel.gridwidth = 3;
+        gbc_infoLabel.insets = new Insets(0, 0, 0, 5);
+        gbc_infoLabel.gridx = 0;
+        gbc_infoLabel.gridy = 0;
+        baixo.add(infoLabel, gbc_infoLabel);
+        
+        // Uma toolbar lateral com botões para executar diferentes funções
+        JPanel toolbar = new JPanel();
+        toolbar.setLayout(new GridLayout(0, 2));
+
+        // A toolbar é adicionado a um flow com layout para espaçamento
+        JPanel flow = new JPanel();
+        flow.add(toolbar);
+
+        contentPane.add(flow, BorderLayout.WEST);
+
+        frame.pack();
+        
+        addImgLogo();
+        /* Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setLocation(d.width/2 - frame.getWidth()/2, d.height/2 - frame.getHeight()/2);
+        frame.setVisible(true); */
+    }
+    
+    private void addImgLogo() {
+        //BufferedImage dimg = img.getScaledInstance(label.width, label.height, Image.SCALE_SMOOTH);
+        
+        ImageIcon logoImg = new ImageIcon(getClass().getResource("logo.png"));
+        
+        Image image = logoImg.getImage();  
+        Image newimg = image.getScaledInstance(100, 24,  java.awt.Image.SCALE_SMOOTH);
+        logoImg = new ImageIcon(newimg);
+        
+        lblImgLogo = new JLabel(logoImg);
+        GridBagConstraints gbc_lblImgLogo = new GridBagConstraints();
+        gbc_lblImgLogo.anchor = GridBagConstraints.EAST;
+        gbc_lblImgLogo.gridx = 4;
+        gbc_lblImgLogo.gridy = 0;
+        baixo.add(lblImgLogo, gbc_lblImgLogo);
+    }
+
+    private void makeMenuBar(JFrame frame) {};
+}
---
+++ src/divcon/TelaLogin.java
@@ -0,0 +1,165 @@
+package divcon;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.Font;
+import javax.swing.JTextField;
+import java.awt.Color;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.Dialog.ModalExclusionType;
+import java.awt.Dialog.ModalityType;
+
+public class TelaLogin extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField txtFieldNome;
+	private JTextField txtFieldSaldo;
+	private JLabel lblInfo;
+	private JComboBox cadastradosBox;
+
+	/**
+	 * Create the dialog.
+	 */
+	public TelaLogin() {
+		setModal(true);
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		
+		setBounds(100, 100, 435, 420);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		GridBagLayout gbl_contentPanel = new GridBagLayout();
+		gbl_contentPanel.columnWidths = new int[] {60, 60, 110, 60, 60};
+		gbl_contentPanel.rowHeights = new int[] {40, 40, 40, 40, 40, 40, 40, 40, 40};
+		gbl_contentPanel.columnWeights = new double[]{0.0, 0.0, 1.0, 0.0, 0.0};
+		gbl_contentPanel.rowWeights = new double[]{0.0, 0.0, 0.0};
+		contentPanel.setLayout(gbl_contentPanel);
+		{
+			JLabel lblDigiteNome = new JLabel("Digite o nome:");
+			lblDigiteNome.setFont(new Font("Tahoma", Font.PLAIN, 14));
+			GridBagConstraints gbc_lblDigiteNome = new GridBagConstraints();
+			gbc_lblDigiteNome.anchor = GridBagConstraints.SOUTH;
+			gbc_lblDigiteNome.insets = new Insets(0, 0, 5, 5);
+			gbc_lblDigiteNome.gridx = 2;
+			gbc_lblDigiteNome.gridy = 0;
+			contentPanel.add(lblDigiteNome, gbc_lblDigiteNome);
+		}
+		{
+			txtFieldNome = new JTextField();
+			GridBagConstraints gbc_textFieldNome = new GridBagConstraints();
+			gbc_textFieldNome.insets = new Insets(0, 0, 5, 5);
+			gbc_textFieldNome.fill = GridBagConstraints.HORIZONTAL;
+			gbc_textFieldNome.gridx = 2;
+			gbc_textFieldNome.gridy = 1;
+			contentPanel.add(txtFieldNome, gbc_textFieldNome);
+			txtFieldNome.setColumns(10);
+		}
+		{
+			JLabel lblSaldo = new JLabel("Digite o saldo:");
+			lblSaldo.setFont(new Font("Tahoma", Font.PLAIN, 14));
+			GridBagConstraints gbc_lblSaldo = new GridBagConstraints();
+			gbc_lblSaldo.anchor = GridBagConstraints.SOUTH;
+			gbc_lblSaldo.insets = new Insets(0, 0, 5, 5);
+			gbc_lblSaldo.gridx = 2;
+			gbc_lblSaldo.gridy = 2;
+			contentPanel.add(lblSaldo, gbc_lblSaldo);
+		}
+		{
+			txtFieldSaldo = new JTextField();
+			GridBagConstraints gbc_textFieldSaldo = new GridBagConstraints();
+			gbc_textFieldSaldo.insets = new Insets(0, 0, 5, 5);
+			gbc_textFieldSaldo.fill = GridBagConstraints.HORIZONTAL;
+			gbc_textFieldSaldo.gridx = 2;
+			gbc_textFieldSaldo.gridy = 3;
+			contentPanel.add(txtFieldSaldo, gbc_textFieldSaldo);
+			txtFieldSaldo.setColumns(10);
+		}
+		{
+			JLabel lblUsrCadastrados = new JLabel("Usuários cadastrados");
+			lblUsrCadastrados.setFont(new Font("Tahoma", Font.PLAIN, 14));
+			GridBagConstraints gbc_lblUsrCadastrados = new GridBagConstraints();
+			gbc_lblUsrCadastrados.insets = new Insets(0, 0, 5, 5);
+			gbc_lblUsrCadastrados.gridx = 2;
+			gbc_lblUsrCadastrados.gridy = 5;
+			contentPanel.add(lblUsrCadastrados, gbc_lblUsrCadastrados);
+		}
+		{
+			cadastradosBox = new JComboBox();
+			cadastradosBox.setMaximumRowCount(20);
+			cadastradosBox.setModel(new DefaultComboBoxModel(new String[] {"-"}));
+			cadastradosBox.setFont(new Font("Tahoma", Font.PLAIN, 12));
+			GridBagConstraints gbc_cadastradosBox = new GridBagConstraints();
+			gbc_cadastradosBox.insets = new Insets(0, 0, 5, 5);
+			gbc_cadastradosBox.fill = GridBagConstraints.HORIZONTAL;
+			gbc_cadastradosBox.gridx = 2;
+			gbc_cadastradosBox.gridy = 6;
+			contentPanel.add(cadastradosBox, gbc_cadastradosBox);
+		}
+		{
+			lblInfo = new JLabel("");
+			lblInfo.setForeground(Color.RED);
+			lblInfo.setFont(new Font("Tahoma", Font.BOLD, 12));
+			GridBagConstraints gbc_lblInfo = new GridBagConstraints();
+			gbc_lblInfo.insets = new Insets(0, 0, 0, 5);
+			gbc_lblInfo.gridx = 2;
+			gbc_lblInfo.gridy = 8;
+			contentPanel.add(lblInfo, gbc_lblInfo);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton btnEntrar = new JButton("Entrar");
+				buttonPane.add(btnEntrar);
+				btnEntrar.addActionListener(e -> entrarSistema());
+			}
+			{
+				JButton btnCancelar = new JButton("Cancelar");
+				buttonPane.add(btnCancelar);
+				btnCancelar.addActionListener(e -> limparCampos());
+			}
+		}
+		
+		
+	}
+	
+	public String getNomeDigitado() {
+		return txtFieldNome.getText();
+	}
+	
+	public String getSaldoDigitado() {
+		return txtFieldSaldo.getText();
+	}
+	
+	public void limparCampos() {
+		txtFieldNome.setText("");
+		txtFieldSaldo.setText("");
+	}
+	
+	public String entrarSistema() {
+		String itemSelecionado = cadastradosBox.getSelectedItem().toString();
+		if(itemSelecionado  != "-") {
+			return itemSelecionado;
+		} else {
+			if(!getNomeDigitado().equals("")) {
+				setVisible(false);
+				return getNomeDigitado();
+			} else {
+				lblInfo.setText("Usuário ou saldo inválidos");
+				return null;
+			}
+		}
+	}
+
+}
