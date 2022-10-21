1bbcb595edb29b9f85f09692f78483f544c1d740
Pedro-V <mingas62@hotmail.com>
2022-10-20 21:18:39 -0300

    Novos testes adicionados

---
+++ src/test/ContaTest.java
@@ -0,0 +1,35 @@
+package test;
+
+import org.junit.Test;
+
+public class ContaTest {
+    @Test
+    public void testAddParticipante() {
+
+    }
+
+    @Test
+    public void testGetInfoConta() {
+
+    }
+
+    @Test
+    public void testGetSaldoFormatado() {
+
+    }
+
+    @Test
+    public void testGetStatusFormatado() {
+
+    }
+
+    @Test
+    public void testListParticipantes() {
+
+    }
+
+    @Test
+    public void testPagaServico() {
+
+    }
+}
---
+++ src/test/ServicoTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.junit.Test;
+
+public class ServicoTest {
+    @Test
+    public void testEstaPago() {
+
+    }
+
+    @Test
+    public void testInfoSummary() {
+
+    }
+
+    @Test
+    public void testRecebePagamento() {
+
+    }
+}
